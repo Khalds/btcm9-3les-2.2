025d991bee2d3f570375fbdc1cad575c9b985afe
khald <abv.khallid@mail.ru>
2022-05-25 00:54:07 +0300

    TASK 2 routers

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 const port = 4000
 
 
-app.use(require('./products.route.js'));
-app.use(require('./brands.route.js'));
+app.use(require('./routes/products.route.js'));
+app.use(require('./routes/brands.route.js'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
